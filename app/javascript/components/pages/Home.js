76f0a32f17c8d69c55e4dec0c17204ad83c4c54a
cmnorrisii <cmnorrisjr@gmail.com>
2023-04-13 12:07:57 -0700

    fixes app functionality after merging branches

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,38 +1,54 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
     <>
-      <div className='home-title'>
+      <div className="home-title">
         <h1>WELCOME TO THE YMCJ APARTMENT DEPARTMENT</h1>
         <h3>Have you been having trouble finding your perfect apartment?</h3>
       </div>
-      <div className='app-description'>
-        <p>Young man, there's no need to feel down<br />
-          I said, young man, pick yourself off the ground<br />
-          I said, young man, 'cause you're in a new town<br />
-          There's no need to be unhappy<br />
-          <br/>
-          Young man, there's a place you can go<br/ >
-          I said, young man, when you're short on your dough<br />
-          You can stay there, and I'm sure you will find<br />
-          Many ways to have a good time<br />
+      <div className="app-description">
+        <p>
+          Young man, there's no need to feel down
           <br />
-          It's fun to stay at the Y.M.C.J.<br />
-          It's fun to stay at the Y.M.C.J.<br />
+          I said, young man, pick yourself off the ground
           <br />
-          They have everything for you men to enjoy<br />
-          You can hang out with all the boys<br />
+          I said, young man, 'cause you're in a new town
           <br />
-          It's fun to stay at the Y.M.C.A.<br />
-          It's fun to stay at the Y.M.C.A.<br />
+          There's no need to be unhappy
           <br />
-          You can get yourself clean, you can have a good meal<br />
+          <br />
+          Young man, there's a place you can go
+          <br />
+          I said, young man, when you're short on your dough
+          <br />
+          You can stay there, and I'm sure you will find
+          <br />
+          Many ways to have a good time
+          <br />
+          <br />
+          It's fun to stay at the Y.M.C.J.
+          <br />
+          It's fun to stay at the Y.M.C.J.
+          <br />
+          <br />
+          They have everything for you men to enjoy
+          <br />
+          You can hang out with all the boys
+          <br />
+          <br />
+          It's fun to stay at the Y.M.C.J.
+          <br />
+          It's fun to stay at the Y.M.C.J.
+          <br />
+          <br />
+          You can get yourself clean, you can have a good meal
+          <br />
           You can do what ever you feel
         </p>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
